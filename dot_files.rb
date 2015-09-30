65cedffc5ef0a76e66f0253cee961b7146922fd0
Michael Steger <mjsteger1@gmail.com>
2015-09-30 10:36:02 -0700

    init commit

---
+++ dot_files.rb
@@ -0,0 +1,21 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class DotFiles < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/mjsteger/dot_files/archive/0.0.2.tar.gz"
+  version "0.0.2"
+  sha256 "c877607ee7be7f60061a4f4fa4d670763c4c4660a2a9685a927e5f3365685e38"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+
+    # Remove unrecognized options if warned by configure
+    system "git init"
+    system "./install-script-mac.sh"
+  end
+end
